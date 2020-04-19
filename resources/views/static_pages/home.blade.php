d0fd1e8296133bb8ca362cef7b3c5f6cb37d175a
于大鱼 <401102730@qq.com>
2020-04-19 10:34:10 +0800

    静态文件缓存问题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.default')
 @section('content')
-  <hi>主页</hi>
+  <div class="jumbotron">
+    <h1>Hello Monster</h1>
+    <p class="lead">
+      哈哈哈哈哈哈哈哈哈
+    </p>
+    <p><a class="btn btn-lg btn-success" href="/" role="button">GO</a></p>
+  </div>
   @stop
